b9910b3f094bf1bdd49b76a34de155e176e78d8d
Raul C. Gomes <raul.cgomes92@gmail.com>
2016-08-26 11:37:28 -0300

    Forca pt5 + atvs

--- Forca/forca.c
+++ Forca/forca.c
@@ -1,58 +1,68 @@
 #include <stdio.h>
 #include <string.h>
 
+//Variaveis Globais:
+char secreatWord[20];
+char chutesPossiveis[26];
+int tentativas = 0;
+
 void abertura() {
 	printf("*****************\n");
 	printf("* Jogo da Forca *\n");
 	printf("*****************\n\n");
 }
 
-void chuta(char chutesPossiveis[26], int tentativas) {
+void chuta() {
 	char chuteUser;
 	scanf(" %c", &chuteUser);
 
 	chutesPossiveis[tentativas] = chuteUser;
-	//tentativas++;
+	tentativas++;
+}
+
+int jachutou(char letra) {
+	int achou = 0;
+
+	for(int j = 0; j < tentativas; j++) {
+		if(chutesPossiveis[j] == letra) {
+			achou = 1;
+			break;
+		}
+	}
+
+	return achou;
+}
+
+void desenhaForca() {
+	for(int i = 0; i < strlen(secreatWord); i++) {
+		
+		int achou = jachutou(secreatWord[i]);
+
+		if(achou){
+			printf("%c ", secreatWord[i]);
+		} else {
+			printf("_ ");
+		}
+	}
+
+	printf("\n");
+}
+
+void escolhePalavra() {
+	sprintf(secreatWord, "teste");
 }
 
 int main() {
-	char secreatWord[20];
-	sprintf(secreatWord, "teste");
-
 	int acertou = 0;
 	int enforcou = 0;
 
-	char chutesPossiveis[26];
-	int tentativas = 0;
-
+	escolhePalavra();
 	abertura();
 
 	do {
-		
-		//imprime palavra secreta
-		for(int i = 0; i < strlen(secreatWord); i++) {
-			
-			int achou = 0;
-			
-			//a letra ja foi chutada
-			for(int j = 0; j < tentativas; j++) {
-				if(chutesPossiveis[j] == secreatWord[i]) {
-					achou = 1;
-					break;
-				}
-			}
 
-			if(achou){
-				printf("%c ", secreatWord[i]);
-			} else {
-				printf("_ ");
-			}
-		}
-
-		printf("\n");
-
-		chuta(chutesPossiveis, tentativas);
-		tentativas++;
+		desenhaForca();
+		chuta();
 
 	} while(!acertou && !enforcou);
 }
